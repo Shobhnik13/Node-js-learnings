00d7fc101c0a4204b0682205419595848df0f009
Shobhnik13 <shobhnikw@gmail.com>
2023-03-30 04:50:37 +0530

    html

--- Server.js
+++ Server.js
@@ -1,6 +1,21 @@
+const fs=require('fs')
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log('request made')
+    console.log(req.url,req.method)
+    //sending html file
+    fs.readFile('./views/index.html',(err,data)=>{
+        if(err)
+        {
+            console.log(err);
+        res.end();
+        }
+        else 
+        {
+        // res.write(data);
+        //if we passing only one thing as a data in file then we do not need res.write(data) just pass the data in end function
+        res.end(data);
+        }
+    })
 })
 server.listen(3000,'localhost',()=>{
     console.log('listening requests on port 3000')
